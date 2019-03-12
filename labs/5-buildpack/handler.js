5dc6a51002c84ed0da2320fed899d38ecc8c8b41
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-03-12 21:28:34 +0100

    Use Triggermes aws-labda-runtime instead

---
+++ labs/5-buildpack/handler.js
@@ -0,0 +1,8 @@
+async function justWait() {
+  return new Promise((resolve, reject) => setTimeout(resolve, 100));
+}
+
+module.exports.sayHelloAsync = async (event) => {
+  await justWait();
+  return {hello: event.name};
+};
