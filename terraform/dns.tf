1bbb5031dd41c90957f10e28bd6adf9bc5ef516c
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-08-18 08:53:37 +0200

    dns: remove DNS to enable destroy

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -9,20 +9,25 @@ output "istio_gateway_ip" {
 }
 
 # DNS knative.site
-resource "google_dns_managed_zone" "knative_site" {
-  name        = "knative-site"
-  dns_name    = "knative.site."
-  description = "knative.site DNS zone"
-  project     = "${var.google_project}"
+# resource "google_dns_managed_zone" "knative_site" {
+#   name        = "knative-site"
+#   dns_name    = "knative.site."
+#   description = "knative.site DNS zone"
+#   project     = "${var.google_project}"
+#
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-  lifecycle {
-    prevent_destroy = true
-  }
+data "google_dns_managed_zone" "knative_site" {
+  name    = "knative-site"
+  project = "${var.google_project}"
 }
 
 resource "google_dns_record_set" "knative_site_booster" {
-  name         = "*.booster.${google_dns_managed_zone.knative_site.dns_name}"
-  managed_zone = "${google_dns_managed_zone.knative_site.name}"
+  name         = "*.booster.${data.google_dns_managed_zone.knative_site.dns_name}"
+  managed_zone = "${data.google_dns_managed_zone.knative_site.name}"
   type         = "A"
   ttl          = 60
 
@@ -32,48 +37,53 @@ resource "google_dns_record_set" "knative_site_booster" {
 }
 
 output "knative_site_ns" {
-  value = "${google_dns_managed_zone.knative_site.name_servers}"
+  value = "${data.google_dns_managed_zone.knative_site.name_servers}"
 }
 
 # DNS knative.club
-resource "google_dns_managed_zone" "knative_club" {
-  name        = "knative-club"
-  dns_name    = "knative.club."
-  description = "knative.club DNS zone"
-  project     = "${var.google_project}"
+# resource "google_dns_managed_zone" "knative_club" {
+#   name        = "knative-club"
+#   dns_name    = "knative.club."
+#   description = "knative.club DNS zone"
+#   project     = "${var.google_project}"
+#
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-  lifecycle {
-    prevent_destroy = true
-  }
+data "google_dns_managed_zone" "knative_club" {
+  name    = "knative-club"
+  project = "${var.google_project}"
 }
 
 output "knative_club_ns" {
-  value = "${google_dns_managed_zone.knative_club.name_servers}"
+  value = "${data.google_dns_managed_zone.knative_club.name_servers}"
 }
 
-resource "google_dns_record_set" "knative_club_presentation" {
-  name         = "${google_dns_managed_zone.knative_club.dns_name}"
-  managed_zone = "${google_dns_managed_zone.knative_club.name}"
-  type         = "A"
-  ttl          = 300
-
-  rrdatas = [
-    "185.199.111.153",
-    "185.199.108.153",
-    "185.199.109.153",
-    "185.199.110.153",
-  ]
-
-  project = "${var.google_project}"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#resource "google_dns_record_set" "knative_club_presentation" {
+#  name         = "${google_dns_managed_zone.knative_club.dns_name}"
+#  managed_zone = "${google_dns_managed_zone.knative_club.name}"
+#  type         = "A"
+#  ttl          = 300
+#
+#  rrdatas = [
+#    "185.199.111.153",
+#    "185.199.108.153",
+#    "185.199.109.153",
+#    "185.199.110.153",
+#  ]
+#
+#  project = "${var.google_project}"
+#
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+#}
 
 resource "google_dns_record_set" "knative_club_booster" {
-  name         = "*.booster.${google_dns_managed_zone.knative_club.dns_name}"
-  managed_zone = "${google_dns_managed_zone.knative_club.name}"
+  name         = "*.booster.${data.google_dns_managed_zone.knative_club.dns_name}"
+  managed_zone = "${data.google_dns_managed_zone.knative_club.name}"
   type         = "A"
   ttl          = 60
 
